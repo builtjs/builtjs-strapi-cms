0ce9938e3def922112b12908c4dba66b1ddc5ada
Richard Lovell <richjavalabs@gmail.com>
2021-12-20 21:52:45 +1300

    Updated with mkdir if not exists

--- setup/lib/file-utils.js
+++ setup/lib/file-utils.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const fetch = require("node-fetch");
+const fsp = fs.promises;
 const axios = require("axios");
 const path = require("path");
 
@@ -30,10 +30,13 @@ module.exports = {
       if(!file.path || !file.format || !file.name){
         resolve(null);
       }
-      const imagePath = `${file.path}/${file.name}.${file.format}`;
-      let url = `<RAW_REPO_URL>${imagePath}`;
       let filePath = "../../.data/uploads";
       const fullPath = path.resolve(__dirname, filePath);
+      const imagePath = `${file.path}/${file.name}.${file.format}`;
+      if (!fs.existsSync(fullPath + imagePath)) {
+        fsp.mkdir(fullPath + file.path, { recursive: true });
+      }
+      let url = `<RAW_REPO_URL>${imagePath}`;    
       await downloadImage(url, fullPath + imagePath);
       const size = await this.getFileSizeInBytes(fullPath);
       const mimeType = `image/${file.format === "svg" ? "svg+xml" : file.format}`;
@@ -56,61 +59,3 @@ module.exports = {
     return fileData;
   }
 };
-// const fs = require("fs");
-// const fetch = require("node-fetch");
-// const axios = require("axios");
-// const path = require("path");
-// const request = require('request')
-
-
-// async function downloadImage(url, fullPath) {
-//   return new Promise((resolve, reject) => {
-//     axios({
-//       url,
-//       responseType: "stream",
-//     }).then((response) =>
-//       response.data
-//         .pipe(fs.createWriteStream(fullPath))
-//         .on("finish", () => {
-//           resolve();
-//         })
-//         .on("error", (e) => reject(e))
-//     );
-//   });
-// }
-
-// module.exports = {
-//   getFileSizeInBytes: async function (filePath) {
-//     const stats = fs.statSync(filePath);
-//     const fileSizeInBytes = stats["size"];
-//     return fileSizeInBytes;
-//   },
-//   getFileData: function (fileName) {
-//     return new Promise(async (resolve) => {
-//       let url = `<RAW_REPO_URL>${fileName}`;
-//       let filePath = "../../.data/uploads";
-//       const fullPath = path.resolve(__dirname, filePath, fileName);
-//       await downloadImage(url, fullPath);
-//       // Parse the file metadata
-//       const size = await this.getFileSizeInBytes(fullPath);
-//       const ext = fileName.split(".").pop();
-//       const mimeType = `image/${ext === "svg" ? "svg+xml" : ext}`;
-//       resolve({
-//         path: `./.data/uploads/${fileName}`,
-//         name: fileName,
-//         size,
-//         type: mimeType,
-//       });
-//     });
-//   },
-//   getFilesData: async function(files){
-//     const fileData = {};
-//     if (files && files.length) {
-//       for (let i = 0; i < files.length; i++) {
-//         const file = files[i];
-//         fileData[file.attributeName || file.name] = await this.getFileData(file.fileName);
-//       }
-//     }
-//     return fileData;
-//   }
-// };
