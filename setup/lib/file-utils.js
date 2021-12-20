d046f47fa8f824f8453e0d66dcbadee2c78ba6be
Richard Lovell <richjavalabs@gmail.com>
2021-12-20 20:56:43 +1300

    Updated fileutils

--- setup/lib/file-utils.js
+++ setup/lib/file-utils.js
@@ -2,8 +2,6 @@ const fs = require("fs");
 const fetch = require("node-fetch");
 const axios = require("axios");
 const path = require("path");
-const request = require('request')
-
 
 async function downloadImage(url, fullPath) {
   return new Promise((resolve, reject) => {
@@ -27,19 +25,21 @@ module.exports = {
     const fileSizeInBytes = stats["size"];
     return fileSizeInBytes;
   },
-  getFileData: function (fileName) {
+  getFileData: function (file) {
     return new Promise(async (resolve) => {
-      let url = `<RAW_REPO_URL>${fileName}`;
+      if(!file.path || !file.format || !file.name){
+        resolve(null);
+      }
+      const imagePath = `${file.path}/${file.name}.${file.format}`;
+      let url = `<RAW_REPO_URL>${imagePath}`;
       let filePath = "../../.data/uploads";
-      const fullPath = path.resolve(__dirname, filePath, fileName);
-      await downloadImage(url, fullPath);
-      // Parse the file metadata
+      const fullPath = path.resolve(__dirname, filePath);
+      await downloadImage(url, fullPath + imagePath);
       const size = await this.getFileSizeInBytes(fullPath);
-      const ext = fileName.split(".").pop();
-      const mimeType = `image/${ext === "svg" ? "svg+xml" : ext}`;
+      const mimeType = `image/${file.format === "svg" ? "svg+xml" : file.format}`;
       resolve({
-        path: `./.data/uploads/${fileName}`,
-        name: fileName,
+        path: `./.data/uploads${imagePath}`,
+        name: file.name,
         size,
         type: mimeType,
       });
@@ -50,9 +50,67 @@ module.exports = {
     if (files && files.length) {
       for (let i = 0; i < files.length; i++) {
         const file = files[i];
-        fileData[file.attributeName || file.name] = await this.getFileData(file.fileName);
+        fileData[file.attributeName || file.name] = await this.getFileData(file);
       }
     }
     return fileData;
   }
 };
+// const fs = require("fs");
+// const fetch = require("node-fetch");
+// const axios = require("axios");
+// const path = require("path");
+// const request = require('request')
+
+
+// async function downloadImage(url, fullPath) {
+//   return new Promise((resolve, reject) => {
+//     axios({
+//       url,
+//       responseType: "stream",
+//     }).then((response) =>
+//       response.data
+//         .pipe(fs.createWriteStream(fullPath))
+//         .on("finish", () => {
+//           resolve();
+//         })
+//         .on("error", (e) => reject(e))
+//     );
+//   });
+// }
+
+// module.exports = {
+//   getFileSizeInBytes: async function (filePath) {
+//     const stats = fs.statSync(filePath);
+//     const fileSizeInBytes = stats["size"];
+//     return fileSizeInBytes;
+//   },
+//   getFileData: function (fileName) {
+//     return new Promise(async (resolve) => {
+//       let url = `<RAW_REPO_URL>${fileName}`;
+//       let filePath = "../../.data/uploads";
+//       const fullPath = path.resolve(__dirname, filePath, fileName);
+//       await downloadImage(url, fullPath);
+//       // Parse the file metadata
+//       const size = await this.getFileSizeInBytes(fullPath);
+//       const ext = fileName.split(".").pop();
+//       const mimeType = `image/${ext === "svg" ? "svg+xml" : ext}`;
+//       resolve({
+//         path: `./.data/uploads/${fileName}`,
+//         name: fileName,
+//         size,
+//         type: mimeType,
+//       });
+//     });
+//   },
+//   getFilesData: async function(files){
+//     const fileData = {};
+//     if (files && files.length) {
+//       for (let i = 0; i < files.length; i++) {
+//         const file = files[i];
+//         fileData[file.attributeName || file.name] = await this.getFileData(file.fileName);
+//       }
+//     }
+//     return fileData;
+//   }
+// };
