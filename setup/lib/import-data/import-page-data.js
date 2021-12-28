1c6f38622535a0c13c9a59f03aea7590e7061871
Richard Lovell <richjavalabs@gmail.com>
2021-12-28 16:55:17 +1300

    Added component image upload functionality

--- setup/lib/import-data/import-page-data.js
+++ setup/lib/import-data/import-page-data.js
@@ -1,31 +1,60 @@
-const setup = require("..");
 const createEntry = require("../create-entry");
 const fileUtils = require("../file-utils");
 const { pages } = require("./../../../.data/data.json");
 
 async function importPageData(strapi) {
-  const setup = require("..");
-  const createEntry = require("../create-entry");
-  const fileUtils = require("../file-utils");
-  const { pages } = require("./../../../.data/data.json");
-
-  async function importPageData(strapi) {
-    if (!pages) {
-      return;
+  if (!pages) {
+    return;
+  }
+  for (let i = 0; i < pages.length; i++) {
+    const pageData = pages[i];
+    for (let i = 0; i < pageData.data.contentSections.length; i++) {
+      const contentSection = pageData.data.contentSections[i];
+      pageData.data.contentSections[i] = await uploadComponentFiles(
+        contentSection,
+        pageData
+      );
     }
-    for (let i = 0; i < pages.length; i++) {
-      const page = pages[i];
-      const files = await fileUtils.getFilesData(page.files);
-      await createEntry(strapi, page.collectionName, page.data, files);
-    }
+    const files = await fileUtils.getFilesData(pageData.files.page);
+    await createEntry(strapi, pageData.collectionName, pageData.data, files);
   }
 
-  module.exports = importPageData;
-
-  for (let i = 0; i < pages.length; i++) {
-    const page = pages[i];
-    const files = await fileUtils.getFilesData(page.files);
-    await createEntry(strapi, page.collectionName, page.data, files);
+  async function uploadComponentFiles(obj, pageData) {
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        if (
+          prop !== "__component" &&
+          pageData.files.components &&
+          pageData.files.components[prop]
+        ) {
+          let componentFilesData = obj[prop];
+          for (var fileProp in pageData.files.components[prop]) {
+            if (pageData.files.components[prop].hasOwnProperty(fileProp)) {
+              let fileData = pageData.files.components[prop][fileProp];
+              if (!Array.isArray(pageData.files.components[prop][fileProp])) {
+                fileData = [pageData.files.components[prop][fileProp]];
+              }
+              let filesObj = await fileUtils.getFilesData(fileData);
+              var files = Object.keys(filesObj).map((key) => {
+                return filesObj[key];
+              });
+              let uploads = await strapi.plugins.upload.services.upload.upload({
+                data: {},
+                files: files,
+              });
+              if (Array.isArray(componentFilesData)) {
+                for (let i = 0; i < componentFilesData.length; i++) {
+                  componentFilesData[i][fileProp] = uploads[i].id;
+                }
+              } else {
+                componentFilesData[fileProp] = uploads[0].id;
+              }
+            }
+          }
+        }
+      }
+    }
+    return obj;
   }
 }
 
