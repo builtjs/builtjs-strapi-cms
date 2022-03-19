4732c02c1ed34a982d8d5780957b1b4e0e902241
Richard Lovell <richjavalabs@gmail.com>
2022-03-20 11:59:43 +1300

    Updated postgres config

--- config/database.js
+++ config/database.js
@@ -1,23 +1,44 @@
 const path = require('path');
+const parse = require("pg-connection-string").parse;
 
 module.exports = ({ env }) => {
   if (env('NODE_ENV') === 'production') {
+    const { host, port, database, user, password } = parse(
+      process.env.DATABASE_URL
+    );
     return {
       connection: {
-        client: 'postgres',
+        client: "postgres",
         connection: {
-          host: env('DATABASE_HOST', '127.0.0.1'),
-          port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi'),
-          user: env('DATABASE_USERNAME', 'strapi'),
-          password: env('DATABASE_PASSWORD', 'strapi'),
+          host,
+          port,
+          database,
+          user,
+          password,
           ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+            rejectUnauthorized: false,
           },
         },
         debug: false,
       },
     };
+
+    // return {
+    //   connection: {
+    //     client: 'postgres',
+    //     connection: {
+    //       host: env('DATABASE_HOST', '127.0.0.1'),
+    //       port: env.int('DATABASE_PORT', 5432),
+    //       database: env('DATABASE_NAME', 'strapi'),
+    //       user: env('DATABASE_USERNAME', 'strapi'),
+    //       password: env('DATABASE_PASSWORD', 'strapi'),
+    //       ssl: {
+    //         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+    //       },
+    //     },
+    //     debug: false,
+    //   },
+    // };
   }
 
   return {
