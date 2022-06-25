621ddbecf96118a97b0678d6c9c9e1ad1a29d449
Richard Lovell <richjavalabs@gmail.com>
2022-06-25 18:21:44 +1200

    Bumped to version 4.2.0

--- src/api/greeting/controllers/greeting.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-  /**
-   * greeting controller.
-   */
-  
-  const { createCoreController } = require('@strapi/strapi').factories;
-  
-  module.exports = createCoreController('api::greeting.greeting');
--- src/api/greeting/services/greeting.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * greeting service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::greeting.greeting');
