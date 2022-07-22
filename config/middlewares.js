19e1af6ac946852e6ce7fe93a78adaa04543302e
Richard Lovell <richjavalabs@gmail.com>
2022-07-22 20:10:21 +1200

    Updated for Cloudinary and Heroku

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,11 +1,32 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com", // cloudinary images
+            "lh3.googleusercontent.com", // google avatars
+            "platform-lookaside.fbsbx.com", // facebook avatars
+            "dl.airtable.com", // strapi marketplace
+          ],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public",
 ];
